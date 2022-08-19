b7c588f599c1ca4d58bc1d18ad27b3cc6b8f7ba7
aswayo <comicolon@naver.com>
2022-08-19 15:32:49 +0900

    게시판 오류해결 2

--- resources/views/covidInfo/covidNews/covidNewsIndex.blade.php
+++ resources/views/covidInfo/covidNews/covidNewsIndex.blade.php
@@ -5,18 +5,20 @@
     <div class="mx-auto my-auto px-8 py-4">
         <div class="flex">
             <h1 class="font-bold text-3xl p-3 flex-1">코로나 뉴스</h1>
-            @if(Auth::user()->grade >=10)
-            <a href="{{route('covidNewsCreate')}}" class="flex items-center">
-            <button class="bg-green-400 hover:bg-green-500 px-4 py-2 float-right
-                         text-white rounded-md">글쓰기</button>
-            </a>
-            @endif
+            @auth
+                @if(Auth::user()->grade >=10)
+                <a href="{{route('covidNewsCreate')}}" class="flex items-center">
+                <button class="bg-green-400 hover:bg-green-500 px-4 py-2 float-right
+                             text-white rounded-md">글쓰기</button>
+                </a>
+                @endif
+            @endauth
         </div>
         <ul>
             @foreach ($covidNews as $news)
                 <a href="/covidNews/{{$news->id}}">
-                    <li class="block border-b p-3 m-3"> {{$news->title}}
-                        <small class="float-right"> {{$news->created_at->format('Y-m-d')}}</small></li><br>
+                    <li class="block border-b m-3"> {{$news->title}}
+                        <small class="float-right"> {{$news->created_at->format('Y-m-d')}}</small></li>
                 </a>
             @endforeach
         </ul>
