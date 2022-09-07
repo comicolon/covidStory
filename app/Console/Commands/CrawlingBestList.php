d8a18f63b065f512ed96afae04118f35adf3b065
jung-hyun <48816752+comicolon@users.noreply.github.com>
2022-09-08 04:58:17 +0900

    베스트 모아 백단 작업8

--- app/Console/Commands/CrawlingBestList.php
+++ app/Console/Commands/CrawlingBestList.php
@@ -638,12 +638,15 @@ class CrawlingBestList extends Command
 
         $res = [];
         // 열기
-        try {
-            $client = new GuzzleClient();
-            $response = $client->request('GET', $url);
-        } catch (GuzzleException $e) {
-            die('Exception');
-        }
+
+        $client = new GuzzleClient();
+        $response = $client->request('GET', $url, [
+            'headers' => [
+                'user-agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36',
+                'accept' => 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9'
+            ]
+        ]);
+
 
         if ($response->getStatusCode() == 200) {
             // 찾기
