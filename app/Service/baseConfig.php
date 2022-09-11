90a0dea8a7219bebb80c38e802def4796a6cbe61
jung-hyun <48816752+comicolon@users.noreply.github.com>
2022-09-12 01:05:53 +0900

    베스트 모아 사이트별 계수 설정

--- app/Service/baseConfig.php
+++ app/Service/baseConfig.php
@@ -18,13 +18,13 @@ class baseConfig
     const coeffi_bbdream = 5.053342937;
     const coeffi_clien = 2.337513587;
     const coeffi_dcinside = 1.974541024;
-    const coeffi_fmkorea = 0.3588577;
+    const coeffi_fmkorea = 0.3088577;
     const coeffi_huniv = 1;
     const coeffi_instiz = 1.400360826;
     const coeffi_inven = 0.482877038;
     const coeffi_natepann = 4.898599001;
     const coeffi_ppomppu = 2.357625294;
-    const coeffi_ruliweb = 0.506677348;
+    const coeffi_ruliweb = 0.406677348;
     const coeffi_slrclub = 11.80992894;
     const coeffi_theqoo = 0.62999411;
 }
