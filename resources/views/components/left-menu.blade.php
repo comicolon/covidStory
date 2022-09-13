1b4822db08b4cd1a1b6a40fadfa2a557928e1495
jung-hyun <48816752+comicolon@users.noreply.github.com>
2022-09-13 21:07:19 +0900

    구글 광고 사이드바 1

--- resources/views/components/left-menu.blade.php
+++ resources/views/components/left-menu.blade.php
@@ -15,11 +15,13 @@
     <div class="w-72">
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7981523207325760"
                 crossorigin="anonymous"></script>
+        <!-- 세로 광고 -->
         <ins class="adsbygoogle"
              style="display:block"
-             data-ad-format="autorelaxed"
              data-ad-client="ca-pub-7981523207325760"
-             data-ad-slot="4403572140"></ins>
+             data-ad-slot="9862399494"
+             data-ad-format="auto"
+             data-full-width-responsive="true"></ins>
         <script>
             (adsbygoogle = window.adsbygoogle || []).push({});
         </script>
