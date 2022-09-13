da49299b833a5f13ba62894199e8c84a55ccba0c
jung-hyun <48816752+comicolon@users.noreply.github.com>
2022-09-13 20:26:16 +0900

    ads.txt 업로드

--- resources/views/components/left-menu.blade.php
+++ resources/views/components/left-menu.blade.php
@@ -11,4 +11,15 @@
             </li>
         </a>
     </ul>
+    {{--  구글 광고  --}}
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7981523207325760"
+            crossorigin="anonymous"></script>
+    <ins class="adsbygoogle"
+         style="display:block"
+         data-ad-format="autorelaxed"
+         data-ad-client="ca-pub-7981523207325760"
+         data-ad-slot="4403572140"></ins>
+    <script>
+        (adsbygoogle = window.adsbygoogle || []).push({});
+    </script>
 </div>
--- resources/views/components/right-menu.blade.php
+++ resources/views/components/right-menu.blade.php
@@ -1,3 +1,13 @@
 <div class="w-72 hidden md:flex">
-
+    {{--  구글 광고  --}}
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7981523207325760"
+            crossorigin="anonymous"></script>
+    <ins class="adsbygoogle"
+         style="display:block"
+         data-ad-format="autorelaxed"
+         data-ad-client="ca-pub-7981523207325760"
+         data-ad-slot="4403572140"></ins>
+    <script>
+        (adsbygoogle = window.adsbygoogle || []).push({});
+    </script>
 </div>
