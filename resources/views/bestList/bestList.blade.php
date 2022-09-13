13582f0bebff7c8c16822cf8095890bd8f4450c5
jung-hyun <48816752+comicolon@users.noreply.github.com>
2022-09-13 21:28:14 +0900

    구글 광고 사이드바 1

--- resources/views/bestList/bestList.blade.php
+++ resources/views/bestList/bestList.blade.php
@@ -17,6 +17,24 @@
         <div>
             <ul class="">
                 @for($i=0; $i < count($totalList); $i++)
+
+                    @if ($i % 50 === 0)
+                        <div class="max-h-20">
+                            <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7981523207325760"
+                                    crossorigin="anonymous"></script>
+                            <!-- 수평형 글사이 광고 -->
+                            <ins class="adsbygoogle"
+                                 style="display:block"
+                                 data-ad-client="ca-pub-7981523207325760"
+                                 data-ad-slot="5688981078"
+                                 data-ad-format="auto"
+                                 data-full-width-responsive="true"></ins>
+                            <script>
+                                (adsbygoogle = window.adsbygoogle || []).push({});
+                            </script>
+                        </div>
+                    @endif
+
                     <li class="flex border-b">
                         <div class="rating_site w-16 text-white">
                             <p class="flex justify-center">{{$i+1}}</p>
@@ -32,7 +50,6 @@
                             </div>
                         </div>
                     </li>
-
 
                 @endfor
             </ul>
